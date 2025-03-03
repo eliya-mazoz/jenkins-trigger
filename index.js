1cc7c02ff306a9fd103f87fae21a3cee906a5931
oferlis1 <ofer.lis@biocatch.com>
2025-03-03 17:07:22 +0200

    striginfying

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ async function triggerJenkinsJob(jobName, params, headers) {
   const isParameterized = await new Promise((resolve, reject) => 
     request(jsonReq, (err, res, body) => {
       if (err) {
-        core.setFailed(err);
+        core.setFailed(JSON.stringify(err));
         core.error(JSON.stringify(err));
         clearTimeout(timer);
         reject();
@@ -50,7 +50,7 @@ async function triggerJenkinsJob(jobName, params, headers) {
   return new Promise((resolve, reject) =>
     request(req, (err, res) => {
       if (err) {
-        core.setFailed(err);
+        core.setFailed(JSON.stringify(err));
         core.error(JSON.stringify(err));
         clearTimeout(timer);
         reject();
@@ -86,12 +86,12 @@ async function getJobStatus(jobName, statusUrl, headers) {
       request(req, (err, res, body) => {
         if (err) {
           clearTimeout(timer);
-          reject(err);
+          reject(JSON.stringify(err));
         }
         try {
         resolve(JSON.parse(body));
         } catch(err) {
-          core.info(`Failed to parse body err: ${err}, body: ${body}`);
+          core.info(`Failed to parse body err: ${JSON.stringify(err)}, body: ${body}`);
           resolve({timestamp: 0}); // try again
         }
       })
@@ -168,8 +168,8 @@ async function main() {
       await waitJenkinsJob(jobName, startTs, queueItemUrl, headers);
     }
   } catch (err) {
-    core.setFailed(err);
-    core.error(err);
+    core.setFailed(JSON.stringify(err));
+    core.error(JSON.stringify(err));
   } finally {
     clearTimeout(timer);
   }
